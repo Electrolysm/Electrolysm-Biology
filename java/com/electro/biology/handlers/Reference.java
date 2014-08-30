ba91be353b27d45020335b97b954b44c3864a2e3
Clarky158 <benclark.btc@gmail.com>
2014-08-30 20:01:56 +0100

    Update Reference.java

--- java/com/electro/biology/handlers/Reference.java
+++ java/com/electro/biology/handlers/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	
 public static final String MODID = "electrolysm_biology";
 public static final String VERSION = "0.1";
-public static final String NAME = "Electrolysm Biology";
+public static final String NAME = "Electrolysm | Biology";
 }
