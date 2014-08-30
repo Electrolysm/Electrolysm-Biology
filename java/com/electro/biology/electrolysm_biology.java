3f17eb16d6eb8eb8873593e86cdb9fece4eff261
Ellio98 <elliottthomas1998@gmail.com>
2014-08-30 19:58:53 +0100

    Added fist bits of code :)

---
+++ java/com/electro/biology/electrolysm_biology.java
@@ -0,0 +1,16 @@
+package com.electro.biology;
+
+import com.electro.biology.handlers.Reference;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class electrolysm_biology {
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+}
+}
