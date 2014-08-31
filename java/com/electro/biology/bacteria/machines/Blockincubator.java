023a51771c72e5318b4690b5115ab01635d48cc7
Clarky158 <benclark.btc@gmail.com>
2014-08-31 19:15:37 +0100

    Update Blockincubator.java

--- java/com/electro/biology/bacteria/machines/Blockincubator.java
+++ java/com/electro/biology/bacteria/machines/Blockincubator.java
@@ -34,5 +34,78 @@ public class Blockincubator extends Block{
 			return blockIcon;
 		}
 	}
+	
+	@Override
+    public void getSubBlocks(Item id, CreativeTabs tab, List list)
+    {
+        list.add(new ItemStack(this, 1, 3));
+    }
+    
+    @Override
+    public void onBlockAdded(World par1World, int par2, int par3, int par4)
+    {
+        super.onBlockAdded(par1World, par2, par3, par4);
+        this.setDefaultDirection(par1World, par2, par3, par4);
+    }
+
+    private void setDefaultDirection(World par1World, int par2, int par3, int par4)
+    {
+        if (!par1World.isRemote)
+        {
+            Block l = par1World.getBlock(par2, par3, par4 - 1);
+            Block i1 = par1World.getBlock(par2, par3, par4 + 1);
+            Block j1 = par1World.getBlock(par2 - 1, par3, par4);
+            Block k1 = par1World.getBlock(par2 + 1, par3, par4);
+            byte b0 = 3;
+
+            if (l.isOpaqueCube() && !i1.isOpaqueCube())
+            {
+                b0 = 3;
+            }
+
+            if (i1.isOpaqueCube() && !l.isOpaqueCube())
+            {
+                b0 = 2;
+            }
+
+            if (j1.isOpaqueCube() && !k1.isOpaqueCube())
+            {
+                b0 = 5;
+            }
+
+            if (k1.isOpaqueCube() && !j1.isOpaqueCube())
+            {
+                b0 = 4;
+            }
+
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, b0, 2);
+        }
+    }
+
+    @Override
+    public void onBlockPlacedBy(World par1World, int par2, int par3, int par4, EntityLivingBase par5EntityLivingBase, ItemStack par6ItemStack)
+    {
+        int l = MathHelper.floor_double((double)(par5EntityLivingBase.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+
+        if (l == 0)
+        {
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, 2, 2);
+        }
+
+        if (l == 1)
+        {
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, 5, 2);
+        }
+
+        if (l == 2)
+        {
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, 3, 2);
+        }
+
+        if (l == 3)
+        {
+            par1World.setBlockMetadataWithNotify(par2, par3, par4, 4, 2);
+        }
+    }
 }
 
