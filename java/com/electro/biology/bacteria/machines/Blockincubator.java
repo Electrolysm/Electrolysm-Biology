e69afcccc8a9ee04ce6e1256b21487a1fc8a5b1f
Ellio98 <elliottthomas1998@gmail.com>
2014-08-31 17:29:43 +0100

    More work on texture for incubator

--- java/com/electro/biology/bacteria/machines/Blockincubator.java
+++ java/com/electro/biology/bacteria/machines/Blockincubator.java
@@ -2,15 +2,24 @@ package com.electro.biology.bacteria.machines;
 
 import com.electro.biology.handlers.Reference;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.util.IIcon;
 
 public class Blockincubator extends Block{
-	
+	public IIcon frontIcon;
 	public Blockincubator()
 	{
 		super(Material.iron);
-		setBlockTextureName(Reference.MODID + ";" + getUnlocalizedName().substring(5));
+		
 	}
-
+@Override 
+@SideOnly(Side.CLIENT)
+public void registerBlockIcons(IIconRegister reg)
+{
+	blockIcon = reg.registerIcon(Reference.MODID + ":incubator");
+}
 }
