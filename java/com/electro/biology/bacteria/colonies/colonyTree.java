d43e1893fef474a18006a4f03ef4569b52f6cb28
Ellio98 <elliottthomas1998@gmail.com>
2014-09-13 20:13:28 +0100

    added textures but minecraft wont load them :/

--- java/com/electro/biology/bacteria/colonies/colonyTree.java
+++ java/com/electro/biology/bacteria/colonies/colonyTree.java
@@ -1,36 +1,28 @@
 package com.electro.biology.bacteria.colonies;
 
 import java.util.ArrayList;
-import java.util.List;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 import com.electro.biology.electrolysm_biology;
 import com.electro.biology.handlers.Reference;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.IIcon;
-import net.minecraft.util.MathHelper;
-import net.minecraft.world.World;
-
 public class colonyTree extends Block{
-	public IIcon frontIcon;
+	
 	public colonyTree()
 	{
-		super(Material.cake);
+		super(Material.leaves);
 		setCreativeTab(electrolysm_biology.TabElectrolysm_Biology);
 		setBlockTextureName(Reference.MODID + ":" + getUnlocalizedName().substring(5));
 		setStepSound(soundTypeGrass);
 		setHardness(1.0F);
 		setResistance(5.0F);
 	}
-	 public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune)
+	 @Override
+	public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune)
 	 {
 		 return new ArrayList<ItemStack>();
 	 }
--- java/com/electro/biology/electrolysm_biology.java
+++ java/com/electro/biology/electrolysm_biology.java
@@ -18,6 +18,7 @@ import com.electro.biology.handlers.TabElectrolysm_Biology;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
 public class electrolysm_biology {
