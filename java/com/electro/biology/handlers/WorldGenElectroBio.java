3245fc156f331917eade62bf7f2215914d913772
Ellio98 <elliottthomas1998@gmail.com>
2014-09-27 20:13:53 +0100

    Finished the ore gen

--- java/com/electro/biology/handlers/WorldGenElectroBio.java
+++ java/com/electro/biology/handlers/WorldGenElectroBio.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import cpw.mods.fml.common.IWorldGenerator;
@@ -16,10 +17,13 @@ public class WorldGenElectroBio implements IWorldGenerator {
 			generateNether(world, rand, chunkX * 16, chunkZ * 16);
 		case 0:	
 			generateSurface(world, rand, chunkX * 16, chunkZ * 16);
+		case 1:
+		generateEnd(world, rand, chunkX * 16, chunkZ * 16);
 		}
 		
 	}
-    private static int getSurface(World world, int x, int z)
+
+	private static int getSurface(World world, int x, int z)
     {
         int height = 256;
         boolean continueQ = true;
@@ -39,9 +43,9 @@ public class WorldGenElectroBio implements IWorldGenerator {
         return height + 1;
     }
     private void generateSurface(World world, Random random, int chunkX, int chunkZ)
-    {
-    	if(world.getgetBiomeGenForCoords(x, z) == BiomeGenBase.grass){
-        	for (int i = 0; i < 0.01; i++)
+    {    	
+    	if(world.getBiomeGenForCoords(chunkX, chunkZ) == BiomeGenBase.plains){
+    		for (int i = 0; i < 0.01; i++)
         	{
             		int xCoord = chunkX + random.nextInt(16);
             		int yCoord = getSurface(world, chunkX + random.nextInt(16), chunkZ + random.nextInt(16));
@@ -49,17 +53,52 @@ public class WorldGenElectroBio implements IWorldGenerator {
             		(new WorldGenMinable(ModBlocks.colonyGrass, 3, Blocks.grass)).generate(world, random, xCoord, yCoord, zCoord);
         	}
     	}
-	for (int i = 0; i < 100; i++)
+    	if(world.getBiomeGenForCoords(chunkX, chunkZ) == BiomeGenBase.swampland){
+        for (int i = 0; i < 0.1; i++)
         {
             int xCoord = chunkX + random.nextInt(16);
             int yCoord = getSurface(world, chunkX + random.nextInt(16), chunkZ + random.nextInt(16));
             int zCoord = chunkZ + random.nextInt(16);
-            (new WorldGenMinable(ModBlocks.colonySwamp, 3, Blocks.waterlily)).generate(world, random, xCoord, yCoord, zCoord);
-       }
+            (new WorldGenMinable(ModBlocks.colonySwamp, 3, Blocks.grass)).generate(world, random, xCoord, yCoord, zCoord);
+        }
+    	}
+        if(world.getBiomeGenForCoords(chunkX, chunkZ) == BiomeGenBase.forest){
+            for (int i = 0; i < 5; i++)
+            {
+                int xCoord = chunkX + random.nextInt(16);
+                int yCoord = getSurface(world, chunkX + random.nextInt(16), chunkZ + random.nextInt(16));
+                int zCoord = chunkZ + random.nextInt(16);
+                (new WorldGenMinable(ModBlocks.colonyTree, 3, Blocks.leaves)).generate(world, random, xCoord, yCoord, zCoord);
+            }
+        }
+                for (int i = 0; i < 1; i++)
+                {
+                    int xCoord = chunkX + random.nextInt(16);
+                    int yCoord = getSurface(world, chunkX + random.nextInt(16), chunkZ + random.nextInt(16));
+                    int zCoord = chunkZ + random.nextInt(16);
+                    (new WorldGenMinable(ModBlocks.colonyIce, 3, Blocks.snow_layer)).generate(world, random, xCoord, yCoord, zCoord);
+
+                }
     }
 
-	private void generateNether(World world, Random rand, int i, int j) {
+	private void generateNether(World world, Random random, int chunkX, int chunkZ) {
 		
+    	for (int i = 0; i < 25; i++)
+    	{
+        		int xCoord = chunkX + random.nextInt(16);
+        		int yCoord = random.nextInt(256);
+        		int zCoord = chunkZ + random.nextInt(16);
+        		(new WorldGenMinable(ModBlocks.colonyNether, 3, Blocks.netherrack)).generate(world, random, xCoord, yCoord, zCoord);
+    	}
+	}
+    private void generateEnd(World world, Random random, int chunkX, int chunkZ) {
+       	for (int i = 0; i < 1; i++)
+    	{
+        		int xCoord = chunkX + random.nextInt(16);
+        		int yCoord = getSurface(world, chunkX + random.nextInt(16), chunkZ + random.nextInt(16));
+        		int zCoord = chunkZ + random.nextInt(16);
+        		(new WorldGenMinable(ModBlocks.colonyEnd, 3, Blocks.end_stone)).generate(world, random, xCoord, yCoord, zCoord);
+	}
 	}
 
 }
