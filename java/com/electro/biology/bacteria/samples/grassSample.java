deeb66458244aee1312c859123b6e73bb2f92070
Ellio98 <elliottthomas1998@gmail.com>
2014-09-29 19:50:55 +0100

    added meta data

--- java/com/electro/biology/bacteria/samples/grassSample.java
+++ java/com/electro/biology/bacteria/samples/grassSample.java
@@ -1,18 +1,31 @@
 package com.electro.biology.bacteria.samples;
 
+import java.util.List;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
 import com.electro.biology.electrolysm_biology;
 import com.electro.biology.handlers.Reference;
 
-import net.minecraft.item.Item;
-
 public class grassSample extends Item {
 	
-	public grassSample()
+  public grassSample() 
 	{
 		super();
 		setUnlocalizedName("grassSample");
-		setTextureName(Reference.MODID + ":" + getUnlocalizedName().substring(5));
 		setCreativeTab(electrolysm_biology.TabElectrolysm_Biology);
+		setTextureName(Reference.MODID + ":" + getUnlocalizedName().substring(5));
+	}
+   
+		
+
+    @Override
+	public void getSubItems(Item item, CreativeTabs tab, List list) {
+		for (int i = 0; i < 7; i ++) {
+			list.add(new ItemStack(item, 1, i));
+		}
 	}
 
-}
+	}
