dc5b8dbbd6f894e636241fb3247ed9c506699add
Ellio98 <elliottthomas1998@gmail.com>
2014-10-28 20:14:19 +0000

    added algae texture

--- java/com/electro/biology/handlers/WorldGenElectroBio.java
+++ java/com/electro/biology/handlers/WorldGenElectroBio.java
@@ -79,8 +79,7 @@ public class WorldGenElectroBio implements IWorldGenerator {
                     (new WorldGenMinable(ModBlocks.colonyIce, 3, Blocks.snow_layer)).generate(world, random, xCoord, yCoord, zCoord);
 
                 }
-	if(world.getBiomeGenForCoords(chunkX, chunkZ) == BiomeGenBase.swampland){
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 500; i++)
     {
         int xCoord = chunkX + random.nextInt(16);
         int yCoord = getSurface(world, chunkX + random.nextInt(16), chunkZ + random.nextInt(16));
@@ -88,7 +87,6 @@ public class WorldGenElectroBio implements IWorldGenerator {
         (new WorldGenMinable(ModBlocks.algae, 3, Blocks.waterlily)).generate(world, random, xCoord, yCoord, zCoord);
     }
 	}
-    }
 
 	private void generateNether(World world, Random random, int chunkX, int chunkZ) {
 		
