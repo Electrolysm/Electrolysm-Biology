1a0e59e75bb15da593618c2e2e16e720a84c99d5
Clarky158 <benclark.btc@gmail.com>
2014-11-01 18:09:11 +0000

    Made changes

--- java/com/electro/biology/bacteria/colonies/algae.java
+++ java/com/electro/biology/bacteria/colonies/algae.java
@@ -1,14 +1,18 @@
 package com.electro.biology.bacteria.colonies;
 
 import java.util.List;
+import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityBoat;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.world.World;
 
 import com.electro.biology.electrolysm_biology;
+import com.electro.biology.handlers.ModItems;
 import com.electro.biology.handlers.Reference;
 
 public class algae extends Block{
@@ -25,6 +29,11 @@ public class algae extends Block{
         float f1 = 0.015625F;
         this.setBlockBounds(0.5F - f, 0.0F, 0.5F - f, 0.5F + f, f1, 0.5F + f);
 	}
+	@Override
+public Item getItemDropped(int metadata, Random rand, int fortune)
+{
+		return ModItems.agar;
+}
     public int getRenderType()
     {
         return 23;
