2229269a75b889e3b806c60c686edbdf6bdd675e
Ellio98 <elliottthomas1998@gmail.com>
2014-11-02 20:36:55 +0000

    more work on identifier gui

--- java/com/electro/biology/bacteria/machines/identifier.java
+++ java/com/electro/biology/bacteria/machines/identifier.java
@@ -127,7 +127,7 @@ public class identifier extends BlockContainer{
 	@Override
 	public TileEntity createNewTileEntity(World p_149915_1_, int p_149915_2_) {
 		// TODO Auto-generated method stub
-		return new TileEntityIdentifier();
+		return new TileEntityIdentifier(2);
 	}
 }
 
