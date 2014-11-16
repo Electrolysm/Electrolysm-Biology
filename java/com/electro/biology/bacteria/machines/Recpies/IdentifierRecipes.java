23eb2964d201b8ccfde47ea453d6ad8cc1b5f02c
Clarky158 <benclark.btc@gmail.com>
2014-11-16 18:52:20 +0000

    Update IdentifierRecipes.java

--- java/com/electro/biology/bacteria/machines/Recpies/IdentifierRecipes.java
+++ java/com/electro/biology/bacteria/machines/Recpies/IdentifierRecipes.java
@@ -9,27 +9,33 @@ import net.minecraft.item.ItemStack;
 
 public class IdentifierRecipes {
 
-public static final IdentifierRecipes idbase = new IdentifierRecipes();
+	public static final IdentifierRecipes idbase = new IdentifierRecipes();
 
-private Map<RecipeStack,RecipeStack> map = new HashMap<RecipeStack,RecipeStack>();
+	private Map<RecipeStack, List<RecipeStack>> map = new HashMap<RecipeStack, List<RecipeStack>>();
 
-public static final IdentifierRecipes id(){
-	return idbase;
-}
-private IdentifierRecipes(){
-	this.addRecipe(new ItemStack(input), new ItemStack(result));
-}
-public void addRecipe(ItemStack input, ItemStack output) {
-	this.map.put(new RecipeStack( input), new RecipeStack(output));
-}
-public ItemStack getResult(ItemStack input){
-	if(input == null){
+	public static final IdentifierRecipes id(){
+		return idbase;
+	}
+	private IdentifierRecipes(){
+		List<ItemStack> recipeList = Arrays.asList(new ItemStack(result1), new ItemStack(result2));
+		this.addRecipe(new ItemStack(input), recipeList);
+	}
+	public void addRecipe(ItemStack input, List<ItemStack> outputs) {
+		List<RecipeStack> list = new ArrayList<RecipeStack>();
+		for(int i = 0; i < outputs.size(); i++){
+			list.add(new RecipeStack(outputs.get(i)));
+		}
+		this.map.put(new RecipeStack( input), list);
+	}
+	public ItemStack getResult(ItemStack input, Random random){
+		if(input == null){
+			return null;
+		}
+		int randomInt = random.nextInt(outputs.size());
+		List<RecipeStack> outputs = this.map.get(new RecipeStack(input));
+		if(outputs!=null && outputs.get(randomInt) != null){
+			return outputs.get(randomInt).getStackValue();
+		}
 		return null;
 	}
-	RecipeStack output = this.map.get(new RecipeStack(input));
-	if(output!=null){
-		return output.getStackValue();
-	}
-	return null;
-}
 }
